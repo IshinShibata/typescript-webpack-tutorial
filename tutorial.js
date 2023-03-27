edac31c010dbb8cb6bee5aba0e9622b684bdadab
柴田維新 <kobukobudesu.dekaihou@gmail.com>
2023-03-28 00:16:01 +0900

    first commit

---
+++ tutorial.js
@@ -0,0 +1,6 @@
+// const a: Number = 3;
+// const b: Number = a;
+// console.log(b)
+var array1 = [1, 2, 3];
+var array2 = [];
+array2 = array1;
---
+++ tutorial.ts
@@ -0,0 +1,7 @@
+// const a: Number = 3;
+// const b: Number = a;
+// console.log(b)
+
+let array1: number[] = [1, 2, 3];
+let array2: number[] = []
+array2 = array1;
